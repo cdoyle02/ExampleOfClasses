4f11d209e7e5cfc5a4c6f2f21c26b99046d39ebc
Charlie Doyle <charlesdoyle002@gmail.com>
2019-09-16 14:23:47 -0500

    working yo

---
+++ ExampleOfClasses/ExampleOfClasses/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleOfClasses
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public Product(string name)
+        {
+            Name = name;
+        }
+        public Product(decimal price)
+        {
+            Price = price;
+        }
+    }
+}
---
+++ ExampleOfClasses/ExampleOfClasses/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExampleOfClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = new Product("pumpkin", 200.00m);
+            //var productPrice = new Product(200);
+
+            
+
+            Console.WriteLine($"The product name is {product.Name} and costs {product.Price}");
+     
+        }
+    }
+}
